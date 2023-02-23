6a8cac506ebb004638ffc1defc24ab8aafa45e32
MallikarjunaRudraniTech <121670751+MallikarjunaRudraniTech@users.noreply.github.com>
2023-02-23 14:40:02 +0530

    Add project files.

---
+++ Controllers/TblCreateLeadsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rudrani_Tech_CRM.Models;
+
+namespace Rudrani_Tech_CRM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TblCreateLeadsController : ControllerBase
+    {
+        private readonly RudraniCrmContext _context;
+
+        public TblCreateLeadsController(RudraniCrmContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TblCreateLeads
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblCreateLead>>> GetTblCreateLeads()
+        {
+          if (_context.TblCreateLeads == null)
+          {
+              return NotFound();
+          }
+            return await _context.TblCreateLeads.ToListAsync();
+        }
+
+        // GET: api/TblCreateLeads/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblCreateLead>> GetTblCreateLead(int id)
+        {
+          if (_context.TblCreateLeads == null)
+          {
+              return NotFound();
+          }
+            var tblCreateLead = await _context.TblCreateLeads.FindAsync(id);
+
+            if (tblCreateLead == null)
+            {
+                return NotFound();
+            }
+
+            return tblCreateLead;
+        }
+
+        // PUT: api/TblCreateLeads/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblCreateLead(int id, TblCreateLead tblCreateLead)
+        {
+            if (id != tblCreateLead.LeadId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblCreateLead).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblCreateLeadExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TblCreateLeads
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblCreateLead>> PostTblCreateLead(TblCreateLead tblCreateLead)
+        {
+          if (_context.TblCreateLeads == null)
+          {
+              return Problem("Entity set 'RudraniCrmContext.TblCreateLeads'  is null.");
+          }
+            _context.TblCreateLeads.Add(tblCreateLead);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TblCreateLeadExists(tblCreateLead.LeadId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTblCreateLead", new { id = tblCreateLead.LeadId }, tblCreateLead);
+        }
+
+        // DELETE: api/TblCreateLeads/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblCreateLead(int id)
+        {
+            if (_context.TblCreateLeads == null)
+            {
+                return NotFound();
+            }
+            var tblCreateLead = await _context.TblCreateLeads.FindAsync(id);
+            if (tblCreateLead == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblCreateLeads.Remove(tblCreateLead);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TblCreateLeadExists(int id)
+        {
+            return (_context.TblCreateLeads?.Any(e => e.LeadId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/RudraniCrmContext.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rudrani_Tech_CRM.Models;
+
+public partial class RudraniCrmContext : DbContext
+{
+    //public RudraniCrmContext()
+    //{
+    //}
+
+    public RudraniCrmContext(DbContextOptions<RudraniCrmContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblCreateLead> TblCreateLeads { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=LAPTOP-6AIBT7VI;Database=rudrani_crm;Integrated Security=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblCreateLead>(entity =>
+        {
+            entity.HasKey(e => e.LeadId).HasName("PK_CreateLead");
+
+            entity.ToTable("tblCreateLead");
+
+            entity.HasIndex(e => e.Email, "UQ_CreateLead_Email").IsUnique();
+
+            entity.HasIndex(e => e.Mobile, "UQ_CreateLead_Mobile").IsUnique();
+
+            entity.Property(e => e.LeadId)
+                .ValueGeneratedNever()
+                .HasColumnName("lead_id");
+            entity.Property(e => e.AnnualRevenue).HasColumnName("annual_revenue");
+            entity.Property(e => e.City)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("city");
+            entity.Property(e => e.Company)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("company");
+            entity.Property(e => e.Country)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("country");
+            entity.Property(e => e.CreationDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("creation_date");
+            entity.Property(e => e.Description)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.EmailOptOut)
+                .HasDefaultValueSql("((0))")
+                .HasColumnName("email_opt_out");
+            entity.Property(e => e.Fax)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("fax");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("first_name");
+            entity.Property(e => e.FirstNameTitle)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("first_name_title");
+            entity.Property(e => e.Industry)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("industry");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("last_name");
+            entity.Property(e => e.LeadImage).HasColumnName("lead_image");
+            entity.Property(e => e.LeadOwner)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("lead_owner");
+            entity.Property(e => e.LeadSource)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("lead_source");
+            entity.Property(e => e.LeadStatus)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("lead_status");
+            entity.Property(e => e.Mobile).HasColumnName("mobile");
+            entity.Property(e => e.NoOfEmployees).HasColumnName("no_of_employees");
+            entity.Property(e => e.Rating)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("rating");
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("role");
+            entity.Property(e => e.SecondaryEmail)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("secondary_email");
+            entity.Property(e => e.SkypeId)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("skype_id");
+            entity.Property(e => e.State)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("state");
+            entity.Property(e => e.Street)
+                .IsUnicode(false)
+                .HasColumnName("street");
+            entity.Property(e => e.Tel).HasColumnName("tel");
+            entity.Property(e => e.Title)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.Twitter)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("twitter");
+            entity.Property(e => e.Website)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("website");
+            entity.Property(e => e.Zipcode).HasColumnName("zipcode");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
