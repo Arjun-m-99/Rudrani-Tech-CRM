6b1c027e5c748a7106f320a52c61d4299e17bd44
MallikarjunaRudraniTech <121670751+MallikarjunaRudraniTech@users.noreply.github.com>
2023-03-07 17:14:24 +0530

    Revert "Dev"

--- Controllers/TblCreateLeadsController.cs
+++ Controllers/TblCreateLeadsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Rudrani_Tech_CRM.DTOs;
 using Rudrani_Tech_CRM.Models;
 
 namespace Rudrani_Tech_CRM.Controllers
@@ -56,12 +55,6 @@ namespace Rudrani_Tech_CRM.Controllers
             Image i = Image.FromStream(ms);           
             tblCreateLead.Profile = i;
             //i..Save(@"c:\s\pic.png", System.Drawing.Imaging.ImageFormat.Png);
-
-            //Convert byte arry to base64string
-            string imreBase64Data = Convert.ToBase64String(img);
-            string imgDataURL = string.Format("data:image/png;base64,{0}", imreBase64Data);
-            tblCreateLead.ImgURL = imgDataURL;
-
             return Ok(tblCreateLead);
         }
 
@@ -99,47 +92,16 @@ namespace Rudrani_Tech_CRM.Controllers
         // POST: api/TblCreateLeads
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<CreateLeadDTO>> PostTblCreateLead(CreateLeadDTO tblCreateLead)
+        public async Task<ActionResult<TblCreateLead>> PostTblCreateLead(TblCreateLead tblCreateLead)
         {
           if (_context.TblCreateLeads == null)
           {
               return Problem("Entity set 'RudraniCrmContext.TblCreateLeads'  is null.");
           }
-            var createLead = new TblCreateLead
-            {
-                Company = tblCreateLead.Company,
-                LeadOwner = tblCreateLead.LeadOwner,
-                FirstNameTitle = tblCreateLead.FirstNameTitle,
-                FirstName = tblCreateLead.FirstName,
-                LastName = tblCreateLead.LastName,
-                Title = tblCreateLead.Title,
-                Email = tblCreateLead.Email,
-                Tel = tblCreateLead.Tel,
-                Fax = tblCreateLead.Fax,
-                Mobile = tblCreateLead.Mobile,
-                Website = tblCreateLead.Website,
-                LeadSource = tblCreateLead.LeadSource,
-                LeadStatus=tblCreateLead.LeadStatus,
-                Industry=tblCreateLead.Industry,
-                NoOfEmployees=tblCreateLead.NoOfEmployees,
-                AnnualRevenue= tblCreateLead.AnnualRevenue,
-                Rating=tblCreateLead.Rating,
-                EmailOptOut=tblCreateLead.EmailOptOut,
-                SkypeId=tblCreateLead.SkypeId,
-                SecondaryEmail=tblCreateLead.SecondaryEmail,
-                Twitter=tblCreateLead.Twitter,
-                Street=tblCreateLead.Street,
-                City=tblCreateLead.City,
-                State=tblCreateLead.State,
-                Zipcode=tblCreateLead.Zipcode,
-                Country=tblCreateLead.Country,
-                Description=tblCreateLead.Description,
-                Role=tblCreateLead.Role,
-            };
-            _context.TblCreateLeads.Add(createLead);
+            _context.TblCreateLeads.Add(tblCreateLead);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetTblCreateLead", new { id = createLead.LeadId }, createLead);
+            return CreatedAtAction("GetTblCreateLead", new { id = tblCreateLead.LeadId }, tblCreateLead);
         }
 
         // DELETE: api/TblCreateLeads/5
